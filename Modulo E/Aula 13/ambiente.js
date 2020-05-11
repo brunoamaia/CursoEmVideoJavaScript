b5fe19de3f32e565e41cbde7869f165140d85e1f
brunoamaia <brunoamaia94@gmail.com>
2020-05-11 09:16:03 -0300

    fim da aula 13

--- Modulo E/Aula 13/ambiente.js
+++ Modulo E/Aula 13/ambiente.js
@@ -1,5 +1,16 @@
-var c = 1
-while (c <= 6) {
+// Estrutura de Repetição com o teste lógico no Início
+
+var c1 = 1
+while (c1 <= 6) {
     console.log('Tudo bem?')
-    c++
+    c1++
 }
+
+
+// Estrutura de Repetição com o teste lógico no Final
+
+var c2 = 1
+do {
+    console.log('Bom dia!!')
+    c2++
+} while ( c2 <= 6)
